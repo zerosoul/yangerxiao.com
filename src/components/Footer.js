fd9a2ea3f318145643ee9a2877de9d2fef6ed8da
Tristan Yang <yanggc888@163.com>
2022-10-31 11:55:46 +0800

    chore: remove link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 const Wrapper = styled.footer`
   width: 100%;
@@ -33,10 +33,10 @@ const Wrapper = styled.footer`
   &.body.is-loading {
     opacity: 0;
   }
-`;
+`
 
 const Footer = (props) => (
-  <Wrapper style={props.timeout ? { display: "none" } : {}}>
+  <Wrapper style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">&copy;2015 - {new Date().getFullYear()}</p>
     <p className="copyright">Created By Tristan With ♥️</p>
     <p className="links">
@@ -47,9 +47,6 @@ const Footer = (props) => (
       <a target="_blank" href="https://eleduck.com/">
         电鸭
       </a>
-      <a target="_blank" href="https://kalacloud.com">
-        卡拉云低代码工具
-      </a>
     </p>
     <p className="icp">
       <a target="_blank" href="http://www.beian.miit.gov.cn/">
@@ -57,10 +54,10 @@ const Footer = (props) => (
       </a>
     </p>
   </Wrapper>
-);
+)
 
 Footer.propTypes = {
-  timeout: PropTypes.bool,
-};
+  timeout: PropTypes.bool
+}
 
-export default Footer;
+export default Footer
